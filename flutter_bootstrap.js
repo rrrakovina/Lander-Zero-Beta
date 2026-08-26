194efa7ba51a0d5a63fb34b753fe43ca68232e7b
rrrakovina <alimpkonst@gmail.com>
2026-08-26 14:09:03 +0000

    Deploying to gh-pages from @ rrrakovina/Lander-Zero-Beta@7186eaa581a7dd6b678d402633d2e06bf0a548be 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3803341310" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1938470726" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
